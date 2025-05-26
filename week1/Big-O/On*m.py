a1af51bfde711e31edf7ca879285082bfcccdbf1
AlukweJonesTerah <jtalukwe@kabarak.ac.ke>
2025-05-26 11:22:50 +0300

    On*m

---
+++ week1/Big-O/On*m.py
@@ -0,0 +1,4 @@
+num1, num2 = [1, 2, 3], [4, 5]
+for i in range(len(num1)):
+    for j in range(len(num2)):
+        print(num1[i], num2[j])
--- week1/Big-O/On^2.py
+++ week1/Big-O/On^2.py
@@ -1,15 +1,23 @@
 # Traversing a square grid
 
-nums = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-for i in range(len(nums)):
-    for j in range(len(nums[i])):
-        print(nums[i][j])
+# nums = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# for i in range(len(nums)):
+#     print("\n", nums[i])
+#     print(i)
+#     for j in range(len(nums[i])):
+#         # print("\n", nums[j])
+#         # print(j)
+#         print(nums[i][j])
         
-for row in nums:
-    for num in row:
-        print("\n", num)
+# for row in nums:
+#     for num in row:
+#         print("\n", num)
         
 nums = [1, 2, 3]
-for i in range(len(nums)):
-    for j in range(i + 1, len(nums)):
-        print(nums[i], nums[j])
+for i in range(len(nums)): # i iterates over the indices [0, 1, 2]
+    # print(i) # provide the index
+    # print(nums[i]) # provides the values in the array
+    for j in range(i + 1, len(nums)): # j iterates from (i + 1) to len(nums)
+        # print(i, j) 
+        print(nums[i], nums[j]) # Prints the value at index i, j
+        
